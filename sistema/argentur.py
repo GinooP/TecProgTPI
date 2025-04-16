43886ec76c25480ecc83e665d70853ff073d3dd8
Gino_Pgh <ginopighin1@gmail.com>
2025-04-16 17:29:13 -0300

    Constructores de las clases

--- sistema/argentur.py
+++ sistema/argentur.py
@@ -1,3 +1,9 @@
+from modelos.servicio import Servicio
+from modelos.pasajero import Pasajero
+
 class Argentur:
-    def __init__(self, sistema_activo:bool):
+    # La empresa tiene un estado(está activo o no) y muchos servicios y ¿¿ pasajeros ??
+    def __init__(self, sistema_activo:bool, pasajeros:list[Pasajero], servicios:list[Servicio]):
         self._sistema_activo = sistema_activo
+        self._pasajeros = pasajeros
+        self._servicios = servicios
