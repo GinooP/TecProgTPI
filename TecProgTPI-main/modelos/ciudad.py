2cdf0712ee184ff6fe03ff46f07f55bc296c88d5
Gino_Pgh <ginopighin1@gmail.com>
2025-04-25 22:35:09 -0300

    Eliminar carpeta clonada

--- TecProgTPI-main/modelos/ciudad.py
+++
@@ -1,6 +0,0 @@
-class Ciudad:
-    # Una ciudad tiene un codigo(identificador), nombre y provincia
-    def __init__(self, codigo:str, nombre:str, provincia:str):
-        self._codigo = codigo
-        self._nombre = nombre
-        self._provincia = provincia
--- TecProgTPI-main/pagos/medioPago.py
+++
@@ -1,4 +0,0 @@
-class MedioPago:
-    # DEFINIR LA INTERFAZ
-    def __init__(self):
-        pass
